7862c8446a163230839e759ee66385910b93260d
yxd3306 <2774284667@qq.com>
2018-08-13 00:40:56 +0800

    添加获取商城首页大广告图片功能

--- taotao-content/taotao-content-interface/src/main/java/com/taotao/content/service/ContentService.java
+++ taotao-content/taotao-content-interface/src/main/java/com/taotao/content/service/ContentService.java
@@ -1,4 +1,15 @@
 package com.taotao.content.service;
 
+import com.taotao.common.utils.TaotaoResult;
+import com.taotao.pojo.TbContent;
+
+import java.util.List;
+
 public interface ContentService {
+
+
+    //添加类目内容
+    TaotaoResult addContent(TbContent content);
+    //获取商城首页大广告图片
+    List<TbContent> getContentByCid(long cid);
 }
