07f1d0949151dab880e44604817f9ed373955c15
yxd3306 <2774284667@qq.com>
2018-08-12 17:09:00 +0800

    新增ContentServiceImpl实现ContentService，功能：管理商品类目内容

---
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -0,0 +1,12 @@
+package com.taotao.content.service.impl;
+
+import com.taotao.common.utils.TaotaoResult;
+import com.taotao.content.service.ContentService;
+import com.taotao.pojo.TbContent;
+
+public class ContentServiceImpl implements ContentService {
+    @Override
+    public TaotaoResult addContent(TbContent content) {
+        return null;
+    }
+}
