5502bc56958e4b70ff6df33114d6868344e5b4b9
yxd3306 <2774284667@qq.com>
2018-08-15 22:37:39 +0800

    添加商品索引接口实现类

---
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/service/impl/SearchItemServiceImpl.java
@@ -0,0 +1,13 @@
+package com.taotao.search.service.impl;
+
+import com.taotao.common.utils.TaotaoResult;
+import com.taotao.search.interfaces.SearchItemService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchItemServiceImpl implements SearchItemService {
+    @Override
+    public TaotaoResult importItemToIndex() {
+        return null;
+    }
+}
